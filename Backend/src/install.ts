08fc7ef2b2f31a18d3b0774a2980d50db19cc8e2
AleksandrSmirnov92 <ryan00@mail.ru>
2022-08-26 15:18:25 +0300

    first commit

---
+++ Backend/src/install.ts
@@ -0,0 +1,3 @@
+// npm i typescript ts-node @types/express @types/node --save-dev установка typescript для backend
+// tsc - компилирует файл
+// tsc -w будет запускать компилятор в автоматическом режими
---
+++ Frontend/bootstrapПодсказки.ts
@@ -0,0 +1,79 @@
+/**
+ если мы хотим чтобы наша сетка начала работать мы можем задать для нашего div
+ <div class = "container"></div>
+ <div class = "container border"></div> - добовляет поля нашему контейнеру
+ <div class = "container-fluid border"></div> - использует контейнер на всю ширину
+ <div class = "container-md border"></div> - точки остановы на 	720 пикселей	960 пикселей	1140 пикселей	1320 пикселей если ниже ничинает себя ввести как fluid тоесть на весь экран
+ остальные свойства https://getbootstrap.com/docs/5.2/layout/containers/
+ Grid(Сетка) - сетка состоит из рядов 
+ <div class = "row"> - создает ряд
+ <div class = "col"></div> создает столбец в итоге у нас получилось один ряд с тремя столбцами
+ <div class = "col"></div>
+ <div class = "col"></div>
+ </div> 
+ <style>
+ [class="col"] {
+    padding:1px,
+    background-color: black,
+    и тд....
+ }
+ </style>
+так же мы можем задавать ширину col-4,5,6 это значит будет соотвествовать ширене 4 5 или 6 единицам 
+https://getbootstrap.com/docs/5.2/layout/grid/
+
+выравнивание 
+https://getbootstrap.com/docs/5.2/layout/columns/
+
+растояние между колонками
+https://getbootstrap.com/docs/5.2/layout/gutters/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
