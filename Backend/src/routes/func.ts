c08a633f78b98bf85f3299d41e09c8c678676037
AleksandrSmirnov92 <ryan00@mail.ru>
2022-09-17 14:48:29 +0300

    ...

--- Backend/src/routes/func.ts
+++ Backend/src/routes/func.ts
@@ -1,4 +1,4 @@
-// import express, { Request, Response } from "express";
+import { Request, Response } from "express";
 const path = require("path");
 const fs = require("fs");
 const AllUser = JSON.parse(
